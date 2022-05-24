46b5d88fa5da1eee1d43c3368e99a8d2ed661758
Dzianis Matsiushyn <ellan@tut.by>
2022-05-24 14:15:04 +0200

    Stylowanie komponentu 'Post' (beta)

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
-import { transformDate } from '../helpers/transformDate';
-
 import axios from 'axios';
+
+import { transformDate } from '../helpers/transformDate';
+import '../css/Post.css';
 
 class Post extends Component {
     constructor(props) {
@@ -16,7 +17,7 @@ class Post extends Component {
 
     postLike = () => {
         const axiosConfig = {
-            headers : {
+            headers: {
                 'Content-Type': 'application/json',
                 'Accept': 'application/json',
                 'Authorization': 'Bearer' + (this.props.currentUser ? this.props.currentUser.jwt_token : null)
@@ -31,50 +32,54 @@ class Post extends Component {
             requestData,
             axiosConfig
         ).then(res => {
-                console.log(res);
+            console.log(res);
 
-                this.setState( (currentState) => {
-                        if(!this.state.liked) {
-                            return { 
-                                liked: res.data.liked,
-                                message: res.data.message,
-                                likesNum: currentState.likesNum + 1
-                            }
-                        }
+            this.setState((currentState) => {
+                if (!this.state.liked) {
+                    return {
+                        liked: res.data.liked,
+                        message: res.data.message,
+                        likesNum: currentState.likesNum + 1
                     }
-                )
-            },
-            error => this.setState( 
-                {message: error.response.data.message } 
+                }
+            }
+            )
+        },
+            error => this.setState(
+                { message: error.response.data.message }
             )
         )
     }
 
     render() {
         return (
-            <div className="user-post-holder" key={this.props.userPost.id}>
-                <div className="post-header">
-                    <div className="avatar-holder">
-                        <img src={this.props.userPost.user.avatar_url} alt="userPhoto" />
-                    </div>
-                    <div className="user-name">
-                        {this.props.userPost.user.username}
-                    </div>
-                    {/* To nie miejsce na ten przycisk */}
-                    <button className="btn follow-btn">
-                        Follow
-                    </button>
+            <div className="container" key={this.props.userPost.id}>
+                <div className="avatar-holder">
+                    <img src={this.props.userPost.user.avatar_url} alt="userPhoto" />
                 </div>
-                <div className="post-content-holder">
-                    <div className="post-content">
-                        {this.props.userPost.content}
+                <div className="user-post-holder">
+                    <div className="post-header">
+                        <div className="user-name">
+                            {this.props.userPost.user.username}
+                            <button className="hide-post">X</button>
+                        </div>
+                        <button className="btn unfollow-btn">
+                            Unfollow
+                        </button>
                     </div>
-                    <div className="post-date">
-                        {transformDate(this.props.userPost.created_at)}
-                    </div>
-                    <div className="post-like">
-                        <button onClick={this.postLike}>Like </button>
-                        <span>{this.state.likesNum}</span>
+                    
+                    
+                    <div className="post-content-holder">
+                        <div className="post-content">
+                            {this.props.userPost.content}
+                        </div>
+                        <div className="post-date">
+                            {transformDate(this.props.userPost.created_at)}
+                        </div>
+                        <div className="post-like">
+                            <button onClick={this.postLike} className="btn">Like </button>
+                            <span>{this.state.likesNum}</span>
+                        </div>
                     </div>
                 </div>
             </div>
